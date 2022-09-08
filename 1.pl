73b1fd8db1d950bbf4939411d50bc686be29b9c1
topcodersonline <43061739+topcodersonline@users.noreply.github.com>
2022-09-08 18:17:20 +0200

    Create 1.pl

---
+++ 1.pl
@@ -0,0 +1,20 @@
+my $host = `hostname`;
+chop($host);
+my $prompt = "$ENV{USER}\@$host";
+my $prompt1 = "$ENV{USER1}\@$host";
+my $prompt2 = "$ENV{USER1}\@$host";
+
+ 
+sub validate_password {
+  my ($password) = @_;
+  my $is_ok = ($password eq "goodpass");
+  printf "$prompt: Password ok? %d\n", $is_ok;
+  return $is_ok;
+};
+ 
+ 
+if (validate_password( $ARGV[0])) {
+  print "$prompt: access granted\n";
+} else {
+  print "$prompt: access denied\n";
+};
